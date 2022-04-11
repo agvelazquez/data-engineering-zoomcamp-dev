586c34145849ccbf65d467f44786ecaf985bd410
velazquez.ags@gmail.com <velazquez.ags@gmail.com>
2022-04-11 00:22:29 +0000

    update fact

---
+++ project/src/dbt/models/fact_russia_losses_equipment.sql
@@ -0,0 +1,7 @@
+{{config(materialized='incremental')}}
+
+SELECT 
+     cast(date as date) as date
+    ,cast(day  as integer) as day
+    ,cast(personnel as integer) as personnel
+FROM {{ source('raw','russia_losses_equipment')}}
